331b09d231ab4953ea47a1994cb44777b5630a03
Diego Díaz Morón <alu0101337975@ull.edu.es>
2024-08-08 15:50:52 +0100

    Secuencia de prueba ejecutada

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
 from pyscript.web import dom
 from pyscript.web.elements import *
+import asyncio
 
+sequence = ["red-btn", "green-btn", "blue-btn", "yellow-btn"]
+
+
+async def start_game():
+    board_events("none")
+    await asyncio.ensure_future(run_sequence())
+    board_events("all")
+
+def board_events(event):
+    dom.find("#simon-board")[0].style["pointer-events"] = event
+
+async def run_sequence():
+    for color in sequence:
+        btn = dom.find("#" + color)[0]
+        btn.id = btn.id + "-highlight"
+        await asyncio.sleep(0.7)
+        btn.id = btn.id.replace("-highlight", "")
+        await asyncio.sleep(0.5)
+
+    
 
 def key_clicked(event):
-    color_name = event.srcElement.id
-    print(color_name)
-    output_element = dom.find("#output")[0]
-    output_element.append(div(color_name))
+    print("clicked")
+
+asyncio.ensure_future(start_game())
